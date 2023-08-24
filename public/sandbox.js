e81429e82f1599387d41fec67e437172891b62b0
Aref Akminasi <137000185+Aref-Akminasi@users.noreply.github.com>
2023-08-24 19:53:55 +0200

    added ts

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,14 @@
 "use strict";
-let age = 23;
+let greet;
+greet = () => {
+    console.log('hello');
+};
+const add = (a, b, c = 10) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(3, 4, '20');
+const minus = (a, b) => {
+    return a - b;
+};
+let result = minus(10, 7);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,18 @@
-let age = 23;
+let greet: Function;
+
+greet = () => {
+  console.log('hello');
+};
+
+const add = (a: number, b: number, c: number | string = 10): void => {
+  console.log(a + b);
+  console.log(c);
+};
+
+add(3, 4, '20');
+
+const minus = (a: number, b: number): number => {
+  return a - b;
+};
+
+let result = minus(10, 7);
