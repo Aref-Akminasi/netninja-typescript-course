3bd4614c7f1dde1c9f8a604ef171c853d6d98425
Aref Akminasi <137000185+Aref-Akminasi@users.noreply.github.com>
2023-08-24 18:38:33 +0200

    added ts

---
+++ sandbox.js
@@ -0,0 +1,10 @@
+//character = 20;
+var character = 'mario';
+var age = 30;
+character = 'luigi';
+age = 40;
+var isBlackBelt = true;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(7.5));
---
+++ sandbox.ts
@@ -0,0 +1,12 @@
+//character = 20;
+let character = 'mario';
+let age = 30;
+character = 'luigi';
+age = 40;
+let isBlackBelt = true;
+
+const circ = (diameter: number) => {
+  return diameter * Math.PI;
+};
+
+console.log(circ(7.5));
