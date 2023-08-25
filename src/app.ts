13a982fd2540c43e829b0774a702e521f85ec611
Aref Akminasi <137000185+Aref-Akminasi@users.noreply.github.com>
2023-08-25 14:43:15 +0200

    added ts

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
 // classes
 class Invoice {
-  client: string;
-  details: string;
-  amount: number;
+  // readonly client: string;
+  // private details: string;
+  // public amount: number;
 
-  constructor(c: string, d: string, a: number) {
-    this.client = c;
-    this.details = d;
-    this.amount = a;
-  }
+  constructor(
+    readonly client: string,
+    private details: string,
+    public amount: number
+  ) {}
 
   format() {
     return `${this.client} owes £${this.amount} for ${this.details}`;
@@ -18,6 +18,12 @@ class Invoice {
 const invOne = new Invoice('Aref', 'Freelance', 200);
 const invTwo = new Invoice('Mario', 'Work', 100);
 let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+
+invoices.forEach((inv) => {
+  console.log(inv.client, inv.amount, inv.format());
+});
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
